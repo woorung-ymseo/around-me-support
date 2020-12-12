05d6a49892d67b7929580d8b67961ba78be6c989
seo <vggn004@gmail.com>
2020-12-12 12:04:59 +0900

    Response 수정

--- src/main/java/com/around/me/support/core/dto/Response.java
+++ src/main/java/com/around/me/support/core/dto/Response.java
@@ -1,5 +1,6 @@
 package com.around.me.support.core.dto;
 
+import com.around.me.support.core.enums.response.ResponseCodeEnum;
 import com.around.me.support.core.support.HttpHeaderBuilder;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,7 @@ public class Response<T> {
     private int status;
 
     @JsonProperty("code")
-    private int code;
+    private String code;
 
     @JsonProperty("message")
     private String message;
@@ -21,7 +22,7 @@ public class Response<T> {
     @JsonProperty("content")
     private T content;
 
-    protected Response(int status, int code, String message, T content) {
+    protected Response(int status, String code, String message, T content) {
         this.status = status;
         this.code = code;
         this.message = message;
@@ -29,42 +30,47 @@ public class Response<T> {
         this.timestamp = System.currentTimeMillis();
     }
 
-    public static <T> Response<T> of(int status, int code, String message, T content) {
+    public static <T> Response<T> of(int status, String code, String message, T content) {
 
         return new Response<>(status, code, message, content);
     }
 
-    public static <T> Response<T> of(int status, int code, String message) {
+    public static <T> Response<T> of(int status, String code, String message) {
 
         return of(status, code, message, null);
     }
 
-    public static <T> Response<T> ok(int code, String message, T content) {
+    public static <T> Response<T> ok(T content) {
+
+        return of(HttpStatus.OK.value(), ResponseCodeEnum.OK.code(), ResponseCodeEnum.OK.message(), content);
+    }
+
+    public static <T> Response<T> ok(String message, T content) {
+
+        return of(HttpStatus.OK.value(), ResponseCodeEnum.OK.code(), message, content);
+    }
+
+    public static <T> Response<T> ok(String code, String message, T content) {
 
         return of(HttpStatus.OK.value(), code, message, content);
     }
 
-    public static <T> Response<T> ok(int code,T content) {
-
-        return of(HttpStatus.OK.value(), code, HttpStatus.OK.getReasonPhrase(), content);
-    }
-
-    public static <T> Response<T> badRequest(int code, T content) {
+    public static <T> Response<T> badRequest(String code, T content) {
 
         return badRequest(code, HttpStatus.BAD_REQUEST.getReasonPhrase(), content);
     }
 
-    public static <T> Response<T> badRequest(int code, String message, T content) {
+    public static <T> Response<T> badRequest(String code, String message, T content) {
 
         return of(HttpStatus.BAD_REQUEST.value(), code, message, content);
     }
 
-    public static <T> Response<T> error(int code, String message, T content) {
+    public static <T> Response<T> error(String code, String message, T content) {
 
         return of(HttpStatus.INTERNAL_SERVER_ERROR.value(), code, message, content);
     }
 
-    public static <T> Response<T> error(int code, T content) {
+    public static <T> Response<T> error(String code, T content) {
 
         return error(code, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), content);
     }
@@ -73,7 +79,7 @@ public class Response<T> {
         return status;
     }
 
-    public int getCode() {
+    public String getCode() {
         return code;
     }
 
